a1a179d9d29c5c8913ca1253633d5c9d65de4d87
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-10-30 13:34:05 +0900

    Refactor/Simplify existing duckdb test

--- spec/adapters/duckdb_spec.rb
+++ spec/adapters/duckdb_spec.rb
@@ -3,18 +3,19 @@ SEQUEL_ADAPTER_TEST = :duckdb
 require_relative 'spec_helper'
 
 describe "A DuckDB database" do
+  after do
+    DB.drop_table?(:items)
+  end
+
   it 'supports auto incrementing primary keys' do
     DB.create_table!(:items) do
       primary_key :id
-      String :name
     end
 
-    DB[:items].insert(:name => 'abc')
-    DB[:items].insert(:name => 'def')
-    DB[:items].insert(:name => 'ghi')
+    DB[:items].insert
+    DB[:items].insert
+    DB[:items].insert
 
     DB[:items].map(:id).sort.must_equal [1, 2, 3]
-  ensure
-    DB.drop_table?(:items)
   end
 end
